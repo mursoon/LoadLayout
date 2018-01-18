e922873ff48d56d2c59d5c748f57dcd0d9542115
Shibalnu <southxvii2@gmail.com>
2018-01-18 11:10:02 +0800

    Initial commit

---
+++ src/main/java/com/shibalnu/loadlayoutmodule/CommonUtils.java
@@ -0,0 +1,13 @@
+package com.shibalnu.loadlayoutmodule;
+
+/**
+ * Created by chenYuXuan on 2018/1/18.
+ * email : southxvii@163.com
+ */
+
+public class CommonUtils {
+
+    public static String getString(){
+        return "Shibalnu";
+    }
+}
